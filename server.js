31cdb4977d49950629695b32f0657bf9c5863166
Sanjeev Midda <110365686+SanjeevMidda@users.noreply.github.com>
2023-06-09 13:41:36 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
-app.get("/", (req, res) => {
-    res.send("Welcome to the homepage");
-});
+// app.get("/", (req, res) => {
+//     res.send("Welcome to the homepage");
+// });
 
-app.get("/fruits", (req,res) => {
+app.get("/", (req,res) => {
     res.sendFile("/Users/sanjeevmidda/Desktop/server-fruits/src/fruitsDB.json")
 })
 app.listen(PORT, () => {
